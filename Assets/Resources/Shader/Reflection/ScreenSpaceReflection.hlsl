3c1907577ce3b718b5f9728a70e3673c9ea9c05c
DengJinYu <56525140+Yami1207@users.noreply.github.com>
2024-08-19 00:47:30 +0800

    优化SSR计算

--- Assets/Resources/Shader/Reflection/ScreenSpaceReflection.hlsl
+++ Assets/Resources/Shader/Reflection/ScreenSpaceReflection.hlsl
@@ -4,7 +4,8 @@
 #include "../../../Shader/Lib/Core.hlsl"
 #include "../../../Shader/Lib/Utils/CameraOpaqueTexture.hlsl"
 
-#define STEP_COUNT 128
+#define REFLECTION_DISTANCE    50.0
+#define STEP_COUNT  200
 
 //--------------------------------------
 // 顶点结构体
@@ -43,6 +44,18 @@ inline float SampleSSRDepth(half2 uv)
     return depth;
 }
 
+inline float4 TransformWorldToHScreen(float3 positionWS, float2 size)
+{
+    float4 positionCS = mul(_VPMatrix, float4(positionWS, 1));
+    float4 positionSS = float4(positionCS.xyz / positionCS.w, positionCS.w);
+    positionSS.xy = 0.5 * positionSS.xy + 0.5;
+#if UNITY_UV_STARTS_AT_TOP
+    positionSS.y = 1 - positionSS.y;
+#endif
+    positionSS.xy *= size;
+    return positionSS;
+}
+
 Varyings vert(Attributes input)
 {
     Varyings output = (Varyings) 0;
@@ -57,13 +70,13 @@ half4 frag(Varyings input) : SV_Target
     if (maskColor.a < 0.5)
         return half4(0, 0, 0, 0);
     float3 normalWS = 2.0 * maskColor.rgb - 1.0;
-
+    
     // 获取屏幕空间
     float depth = SampleSSRDepth(input.texcoord);
-    float4 positionCS = float4(2.0 * input.texcoord - 1.0, depth, 1.0);
 #if UNITY_UV_STARTS_AT_TOP
-	positionCS.y = -positionCS.y;
+	input.texcoord.y = 1.0 - input.texcoord.y;
 #endif
+    float4 positionCS = float4(2.0 * input.texcoord - 1.0, depth, 1.0);
     
     // 屏幕空间转换成世界空间
     float4 positionWS = mul(UNITY_MATRIX_I_VP, positionCS);
@@ -72,31 +85,62 @@ half4 frag(Varyings input) : SV_Target
     // 计算反射向量
     half3 viewDirWS = GetWorldSpaceNormalizeViewDir(positionWS.xyz);
     half3 reflectDirWS = reflect(-viewDirWS, normalWS);
-
+    
+    // 起点和终点（世界空间）
+    float3 startWS = positionWS.xyz;
+    float3 endWS = startWS + REFLECTION_DISTANCE * reflectDirWS;
+    
+    // 齐次屏幕空间坐标
+    float4 texSize = float4(_ScreenParams.xy, 1.0 / _ScreenParams.xy);
+    float4 startSS = TransformWorldToHScreen(startWS, texSize.xy);
+    float4 endSS = TransformWorldToHScreen(endWS, texSize.xy);
+    
+    float3 diff = endSS.xyz - startSS.xyz;
+    float delta = lerp(abs(diff.x), abs(diff.y), step(abs(diff.x), abs(diff.y)));
+    float3 increment = diff / delta;
+    
+    float3 p = startSS.xyz;
     half4 reflectColor = 0;
     UNITY_LOOP
     for (int i = 0; i < STEP_COUNT; ++i)
     {
-        float3 reflectPosWS = positionWS.xyz + reflectDirWS * _Stride * i;
-        float4 reflectPosCS = mul(_VPMatrix, float4(reflectPosWS, 1.0f));
-        float reflectDepth = reflectPosCS.w;
-        
-        reflectPosCS /= reflectPosCS.w;
-        float2 reflectPosSS = reflectPosCS.xy * 0.5 + 0.5;
-#if UNITY_UV_STARTS_AT_TOP
-        reflectPosSS.y = 1.0 - reflectPosSS.y;
-#endif
-        // 判断是否超出屏幕
-        if (reflectPosSS.x < 0.0 || reflectPosSS.y < 0.0 || reflectPosSS.x > 1.0 || reflectPosSS.y > 1.0)
+        p += increment * _Stride;
+        if (p.x < 0.0 || p.y < 0.0 || p.x >= texSize.x || p.y >= texSize.y || p.z <= 0.0 || p.z >= 0.999)
             break;
         
-        float eyeDepth = GetEyeDepth(reflectPosSS);
-        if (reflectDepth > eyeDepth && abs(reflectDepth - eyeDepth) < _Thickness)
+        float2 uv = p.xy * texSize.zw;
+        float depth = SampleSceneDepth(uv);
+        if (depth > p.z && depth - p.z < _Thickness)
         {
-            reflectColor = half4(SampleScreen(reflectPosSS), 1);
+            reflectColor = half4(SampleScreen(uv), 1);
             break;
         }
     }
+
+//    half4 reflectColor = 0;
+//    UNITY_LOOP
+//    for (int i = 0; i < STEP_COUNT; ++i)
+//    {
+//        float3 reflectPosWS = positionWS.xyz + reflectDirWS * _Stride * i;
+//        float4 reflectPosCS = mul(_VPMatrix, float4(reflectPosWS, 1.0f));
+//        float reflectDepth = reflectPosCS.w;
+        
+//        reflectPosCS /= reflectPosCS.w;
+//        float2 reflectPosSS = reflectPosCS.xy * 0.5 + 0.5;
+//#if UNITY_UV_STARTS_AT_TOP
+//        reflectPosSS.y = 1.0 - reflectPosSS.y;
+//#endif
+//        // 判断是否超出屏幕
+//        if (reflectPosSS.x < 0.0 || reflectPosSS.y < 0.0 || reflectPosSS.x > 1.0 || reflectPosSS.y > 1.0)
+//            break;
+        
+//        float eyeDepth = GetEyeDepth(reflectPosSS);
+//        if (reflectDepth > eyeDepth && abs(reflectDepth - eyeDepth) < _Thickness)
+//        {
+//            reflectColor = half4(SampleScreen(reflectPosSS), 1);
+//            break;
+//        }
+//    }
     return reflectColor;
 }
 
